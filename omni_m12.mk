28836bd90fd1679b867c7be0bc807ba20201c27f
xlwnx <105181270+xlwnx@users.noreply.github.com>
2025-01-18 20:58:10 +0700

    Update omni_m12.mk

--- omni_m12.mk
+++ omni_m12.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from m12 device
 $(call inherit-product, device/samsung/m12/device.mk)
 
 PRODUCT_DEVICE := m12
-PRODUCT_NAME := omni_m12
+PRODUCT_NAME := twrp_m12
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M127F
 PRODUCT_MANUFACTURER := samsung
